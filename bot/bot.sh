840c6e79eb0ee7e5036d440d2eedb64e8ba3efbf
msi8888 <128890457+msi8888@users.noreply.github.com>
2024-11-08 20:12:09 +0800

    Update bot.sh

--- bot/bot.sh
+++ bot/bot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 NS=$( cat /etc/xray/dns )
 PUB=$( cat /etc/slowdns/server.pub )
-domain=$(cat /etc/xray/domain)
+domain=$(cat /root/jsphantom/domain)
 #color
 grenbo="\e[92;1m"
 NC='\e[0m'
